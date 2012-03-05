520ab00b044c33163268cce0261a97a67be49e63
Shawn <trisma@gmail.com>
2012-03-05 00:44:00 -0800

    fixed regex for a-list accounts

--- test.js
+++ test.js
@@ -4,18 +4,19 @@ var client = api.createClient()
 
 // Copy your preferred match search's url from the website.
 var matchOptions = {
-	searchUrl: 'http://www.okcupid.com/match?filter1=0,34&filter2=2,23,29&filter3=3,25&filter4=5,604800&filter5=1,0&filter6=35,0&filter7=9,256&filter8=30,4564&locid=0&timekey=1&matchOrderBy=SPECIAL_BLEND&custom_search=0&fromWhoOnline=0&mygender=m&update_prefs=1&sort_type=0&sa=1&using_saved_search='
+	searchUrl: 'http://www.okcupid.com/match?filter1=0,34&filter2=2,23,29&filter3=3,25&filter4=5,31536000&filter5=1,1&filter6=35,2&filter7=9,256&filter8=30,468&filter9=25,8000,10000&locid=0&timekey=1&matchOrderBy=SPECIAL_BLEND&custom_search=0&fromWhoOnline=0&mygender=m&update_prefs=1&sort_type=0&sa=1&using_saved_search='
 }
 
 var automatorOptions = {
 	matchOptions: matchOptions,
 	dryRun: false,
-	maxMessages: 100
+	maxMessages: 100,
+	timeBetweenRequests: 30000
 }
 
 var message = 'How are you?'
 
-client.authenticate('username', 'password', function(success){	
+client.authenticate('user', 'pass', function(success){	
 	automator.messageMatches(client, message, automatorOptions, function(){
 		console.log('Complete!')
 	})
